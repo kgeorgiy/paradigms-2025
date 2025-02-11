aa23f227b15d6004c8844bd5e42c42d33b8e7eb1
Nikolay Vedernikov <VedernikovNV@gmail.com>
2025-02-11 11:43:39 +0300

    + tests 3637 for HW2

--- java/expression/generic/GenericTest.java
+++ java/expression/generic/GenericTest.java
@@ -1,11 +1,9 @@
 package expression.generic;
 
-import base.Named;
 import base.Selector;
+import expression.parser.FullOperations;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.*;
 
 /**
@@ -26,35 +24,144 @@ public final class GenericTest {
         return (int) v;
     }
 
-    private static final Mode<Integer> INTEGER_CHECKED = mode("i", c -> c)
+    private static final GenericTester.Mode.Builder<Integer> INTEGER_CHECKED = mode("i", c -> c)
             .binary("+", (a, b) -> i(a + (long) b))
             .binary("-", (a, b) -> i(a - (long) b))
             .binary("*", (a, b) -> i(a * (long) b))
             .binary("/", (a, b) -> i(a / (long) b))
             .unary("-", a -> i(- (long) a))
+
+            .binary("<", (a, b) -> a < b ? 1 : 0)
+            .binary(">", (a, b) -> a > b ? 1 : 0)
+            .binary("<=", (a, b) -> a <= b ? 1 : 0)
+            .binary(">=", (a, b) -> a >= b ? 1 : 0)
+
+            .binary("==", (a, b) -> a.equals(b) ? 1 : 0)
+            .binary("!=", (a, b) -> !a.equals(b) ? 1 : 0)
+
             ;
 
     // === Doubles
 
-    @SuppressWarnings("Convert2MethodRef")
-    private static final Mode<Double> DOUBLE = mode("d", c -> (double) c)
-            .binary("+", (a, b) -> a + b)
+    private static BinaryOperator<Double> d(final IntPredicate p) {
+        return (a, b) -> p.test(a.compareTo(b)) ? 1.0 : 0.0;
+    }
+
+    private static final GenericTester.Mode.Builder<Double> DOUBLE = mode("d", c -> (double) c)
+            .binary("+", Double::sum)
             .binary("-", (a, b) -> a - b)
             .binary("*", (a, b) -> a * b)
             .binary("/", (a, b) -> a / b)
             .unary("-", a -> -a)
+
+            .binary("<", d(v -> v < 0))
+            .binary(">", d(v -> v > 0))
+            .binary("<=", d(v -> v <= 0))
+            .binary(">=", d(v -> v >= 0))
+            .binary("==", d(v -> v == 0))
+            .binary("!=", d(v -> v != 0))
+
             ;
 
     // === BigIntegers
 
-    private static final Mode<BigInteger> BIG_INTEGER = mode("bi", BigInteger::valueOf)
+    private static BinaryOperator<BigInteger> bi(final IntPredicate p) {
+        return (a, b) -> p.test(a.compareTo(b)) ? BigInteger.ONE : BigInteger.ZERO;
+    }
+
+    private static final GenericTester.Mode.Builder<BigInteger> BIG_INTEGER = mode("bi", BigInteger::valueOf)
             .binary("+", BigInteger::add)
             .binary("-", BigInteger::subtract)
             .binary("*", BigInteger::multiply)
             .binary("/", BigInteger::divide)
             .unary("-", BigInteger::negate)
+
+            .binary("<", bi(v -> v < 0))
+            .binary(">", bi(v -> v > 0))
+            .binary("<=", bi(v -> v <= 0))
+            .binary(">=", bi(v -> v >= 0))
+            .binary("==", bi(v -> v == 0))
+            .binary("!=", bi(v -> v != 0))
+
             ;
 
+
+    // === Parens
+    private static final Consumer<GenericTester> PARENS = tester -> tester.parens("{", "}", "[", "]");
+
+
+    // === Compare
+
+    private static final Consumer<GenericTester> LESS = binary("<", 30);
+    private static final Consumer<GenericTester> GREATER = binary(">", 30);
+    private static final Consumer<GenericTester> LESS_EQ = binary("<=", 30);
+    private static final Consumer<GenericTester> GREATER_EQ = binary(">=", 30);
+    private static final Consumer<GenericTester> EQUAL = binary("==", 20);
+    private static final Consumer<GenericTester> NOT_EQUAL = binary("!=", 20);
+
+
+    // === Unchecked integers
+
+    @SuppressWarnings("Convert2MethodRef")
+    private static final GenericTester.Mode.Builder<Integer> INTEGER_UNCHECKED = mode("u", c -> c)
+            .binary("+", (a, b) -> a + b)
+            .binary("-", (a, b) -> a - b)
+            .binary("*", (a, b) -> a * b)
+            .binary("/", (a, b) -> a / b)
+            .unary("-", a -> -a)
+
+            .binary("<", (a, b) -> a < b ? 1 : 0)
+            .binary(">", (a, b) -> a > b ? 1 : 0)
+            .binary("<=", (a, b) -> a <= b ? 1 : 0)
+            .binary(">=", (a, b) -> a >= b ? 1 : 0)
+            .binary("==", (a, b) -> a.equals(b) ? 1 : 0)
+            .binary("!=", (a, b) -> !a.equals(b) ? 1 : 0)
+
+            ;
+
+
+    // === Longs
+
+    private static final GenericTester.Mode.Builder<Long> LONG = mode("l", c -> (long) c)
+            .binary("+", Long::sum)
+            .binary("-", (a, b) -> a - b)
+            .binary("*", (a, b) -> a * b)
+            .binary("/", (a, b) -> a / b)
+            .unary("-", a -> -a)
+
+            .binary("<", (a, b) -> a < b ? 1L : 0)
+            .binary(">", (a, b) -> a > b ? 1L : 0)
+            .binary("<=", (a, b) -> a <= b ? 1L : 0)
+            .binary(">=", (a, b) -> a >= b ? 1L : 0)
+
+            .binary("==", (a, b) -> a.equals(b) ? 1L : 0)
+            .binary("!=", (a, b) -> !a.equals(b) ? 1L : 0)
+
+            ;
+
+
+    // === Fixed-point integers
+
+    /* package-private */ static final int FIXED = 16;
+    @SuppressWarnings("Convert2MethodRef")
+    private static final GenericTester.Mode.Builder<Integer> INTEGER_FIXED = mode("ifix", a -> a << FIXED)
+            .binary("+", (a, b) -> a + b)
+            .binary("-", (a, b) -> a - b)
+            .binary("*", (a, b) -> (a * b) >> FIXED)
+            .binary("/", (a, b) -> (a / b) << FIXED)
+            .unary("-", a -> -a)
+
+            .binary("<", (a, b) -> a < b ? 1 << FIXED : 0)
+            .binary(">", (a, b) -> a > b ? 1 << FIXED : 0)
+            .binary("<=", (a, b) -> a <= b ? 1 << FIXED : 0)
+            .binary(">=", (a, b) -> a >= b ? 1 << FIXED : 0)
+            .binary("==", (a, b) -> a.equals(b) ? 1 << FIXED : 0)
+            .binary("!=", (a, b) -> !a.equals(b) ? 1 << FIXED : 0)
+
+            ;
+
+
+    // === Common
 
     private GenericTest() {
     }
@@ -69,43 +176,14 @@ public final class GenericTest {
 
     public static final Selector SELECTOR = Selector.composite(GenericTest.class, GenericTester::new, "easy", "hard")
             .variant("Base", INTEGER_CHECKED, DOUBLE, BIG_INTEGER, ADD, SUBTRACT, MULTIPLY, DIVIDE, NEGATE)
+            .variant("3637", PARENS, LESS, LESS_EQ, GREATER, GREATER_EQ, EQUAL, NOT_EQUAL, INTEGER_UNCHECKED, LONG)
             .selector();
 
-    private static <T> Mode<T> mode(final String mode, final IntFunction<T> constant) {
-        return new Mode<>(mode, constant, IntUnaryOperator.identity());
+    private static <T> GenericTester.Mode.Builder<T> mode(final String mode, final IntFunction<T> constant) {
+        return GenericTester.Mode.builder(mode, constant, IntUnaryOperator.identity());
     }
 
     public static void main(final String... args) {
         SELECTOR.main(args);
-    }
-
-
-    /* package-private */ static class Mode<T> implements Consumer<GenericTester> {
-        private final String mode;
-        private final IntFunction<T> constant;
-        private final List<Named<UnaryOperator<GenericTester.F<T>>>> unary = new ArrayList<>();
-        private final List<Named<BinaryOperator<GenericTester.F<T>>>> binary = new ArrayList<>();
-        private final IntUnaryOperator fixer;
-
-        public Mode(final String mode, final IntFunction<T> constant, final IntUnaryOperator fixer) {
-            this.mode = mode;
-            this.constant = constant;
-            this.fixer = fixer;
-        }
-
-        public Mode<T> unary(final String name, final UnaryOperator<T> op) {
-            unary.add(Named.of(name, arg -> (x, y, z) -> op.apply(arg.apply(x, y, z))));
-            return this;
-        }
-
-        public Mode<T> binary(final String name, final BinaryOperator<T> op) {
-            binary.add(Named.of(name, (a, b) -> (x, y, z) -> op.apply(a.apply(x, y, z), b.apply(x, y, z))));
-            return this;
-        }
-
-        @Override
-        public void accept(final GenericTester tester) {
-            tester.mode(mode, constant, unary, binary, fixer);
-        }
     }
 }
